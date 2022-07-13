11d8b73a2ffde7ec821c19acc08d345fcb60c836
Hoelzl Cyprien (I-VU-UEW-MUD - Extern) <cyprien.hoelzl@sbb.ch>
2022-07-13 10:57:53 +0200

    readthedocs bug fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../railFE'))
-from _version import __version__,codename
+from railFE._version import __version__,codename
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
